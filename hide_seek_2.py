4238f3caaaf9ba788962e61c38f73dfd4509257c
Ty Day <tyrday@gmail.com>
2020-10-30 22:57:50 -0400

    plays as blue

--- hide_seek_2.py
+++ hide_seek_2.py
@@ -1,1 +1,9 @@
-import socketio
+# this method isn't going to work at all... duh. 
+# The cell ownership is never shared via html only sockets
+import requests, socketio
+from bs4 import BeautifulSoup
+
+page = requests.get('http://192.168.1.5:5001/seek/copper-dogfish')
+soup = BeautifulSoup(page.text, 'html.parser')
+
+print(soup)
